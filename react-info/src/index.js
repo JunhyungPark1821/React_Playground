23ac19f8d8be3a655628588e973e125d0fc34f54
JunhyungPark1821 <50926511+JunhyungPark1821@users.noreply.github.com>
2023-07-17 10:03:35 -0600

    Parent / Child Components

--- react-info/src/index.js
+++ react-info/src/index.js
@@ -4,16 +4,49 @@ import './index.css';
 import logo from './react-logo.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Header() {
+  return (
+    <header>
+      <nav>
+          <img src={logo} width="40px" alt='logo' />
+      </nav>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2021 Ziroll development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function MainComponent() {
+  return (
+    <div>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+          <li>It's a popular library, so I'll be 
+          able to fit in with the cool kids!</li>
+          <li>I'm more likely to get a job as a developer
+          if I know React</li>
+      </ol>
+    </div>
+  )
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainComponent />
+      <Footer />
+    </div>
+  )
+}
+
 root.render(
-  <div>
-      <img src={logo} alt='logo' width="40px"/>
-      <h1>Fun facts about React</h1>
-      <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-  </div>
+  <Page />
 );
